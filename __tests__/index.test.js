e6ad92ff8456f918c029793ea91db417bad92179
Matveev S <matveevsa987@gmail.com>
2019-12-05 16:49:07 +0300

    complete step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,7 @@ import parser from '../src/utils/parsers';
 
 const tree = 'tree';
 const plain = 'plain';
+const json = 'json';
 const afterJson = parser(`${__dirname}/../__fixtures__/after.json`);
 const beforeJson = parser(`${__dirname}/../__fixtures__/before.json`);
 const afterYml = parser(`${__dirname}/../__fixtures__/after.yaml`);
@@ -12,6 +13,8 @@ const afterIni = parser(`${__dirname}/../__fixtures__/after.ini`);
 const beforeIni = parser(`${__dirname}/../__fixtures__/before.ini`);
 const resultTree = fs.readFileSync(`${__dirname}/../__fixtures__/treeTest.txt`, 'utf-8');
 const resultPlain = fs.readFileSync(`${__dirname}/../__fixtures__/plainTest.txt`, 'utf-8');
+const resultJson = fs.readFileSync(`${__dirname}/../__fixtures__/jsonTest.txt`, 'utf-8');
+const resultJsonIni = fs.readFileSync(`${__dirname}/../__fixtures__/jsonTestIni.txt`, 'utf-8');
 
 test('Config JSON test tree format', () => {
   expect(genDiff(beforeJson, afterJson, tree)).toEqual(resultTree);
@@ -35,4 +38,16 @@ test('Config YAML test Plain format', () => {
 
 test('Config INI test Plain format', () => {
   expect(genDiff(beforeIni, afterIni, plain)).toEqual(resultPlain);
+});
+
+test('Config JSON test JSON format', () => {
+  expect(genDiff(beforeJson, afterJson, json)).toEqual(resultJson);
+});
+
+test('Config YAML test JSON format', () => {
+  expect(genDiff(beforeYml, afterYml, json)).toEqual(resultJson);
+});
+
+test('Config INI test JSON format', () => {
+  expect(genDiff(beforeIni, afterIni, json)).toEqual(resultJsonIni);
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import renderTree from './tree';
 import renderPlain from './plain';
+import renderJson from './json';
 
 const renderFormats = {
   tree: (data) => renderTree(data),
   plain: (data) => renderPlain(data),
+  json: (data) => renderJson(data),
 };
 
 export default (data, format) => renderFormats[format](data);
