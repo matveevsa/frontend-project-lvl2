ddf6bcaf3d3974002f9066f76aacb570273389ff
Matveev S <matveevsa987@gmail.com>
2019-12-05 15:22:22 +0300

    test fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,51 +4,45 @@ import parser from '../src/utils/parsers';
 
 const tree = 'tree';
 const plain = 'plain';
+const afterJson = parser(`${__dirname}/../__fixtures__/after.json`);
+const beforeJson = parser(`${__dirname}/../__fixtures__/before.json`);
+const afterYml = parser(`${__dirname}/../__fixtures__/after.yaml`);
+const beforeYml = parser(`${__dirname}/../__fixtures__/before.yaml`);
+const afterIni = parser(`${__dirname}/../__fixtures__/after.ini`);
+const beforeIni = parser(`${__dirname}/../__fixtures__/before.ini`);
 
 test('Config JSON test tree format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/treeTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.json`);
-  const before = parser(`${__dirname}/../__fixtures__/before.json`);
 
-  expect(genDiff(before, after, tree)).toEqual(result);
+  expect(genDiff(beforeJson, afterJson, tree)).toEqual(result);
 });
 
 test('Config YAML test tree format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/treeTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.yaml`);
-  const before = parser(`${__dirname}/../__fixtures__/before.yaml`);
 
-  expect(genDiff(before, after, tree)).toEqual(result);
+  expect(genDiff(beforeYml, afterYml, tree)).toEqual(result);
 });
 
 test('Config INI test tree format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/treeTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.ini`);
-  const before = parser(`${__dirname}/../__fixtures__/before.ini`);
 
-  expect(genDiff(before, after, tree)).toEqual(result);
+  expect(genDiff(beforeIni, afterIni, tree)).toEqual(result);
 });
 
 test('Config JSON test Plain format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/plainTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.json`);
-  const before = parser(`${__dirname}/../__fixtures__/before.json`);
 
-  expect(genDiff(before, after, plain)).toEqual(result);
+  expect(genDiff(beforeJson, afterJson, plain)).toEqual(result);
 });
 
 test('Config YAML test Plain format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/plainTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.yaml`);
-  const before = parser(`${__dirname}/../__fixtures__/before.yaml`);
 
-  expect(genDiff(before, after, plain)).toEqual(result);
+  expect(genDiff(beforeYml, afterYml, plain)).toEqual(result);
 });
 
 test('Config INI test Plain format', () => {
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/plainTest.txt`, 'utf-8');
-  const after = parser(`${__dirname}/../__fixtures__/after.ini`);
-  const before = parser(`${__dirname}/../__fixtures__/before.ini`);
 
-  expect(genDiff(before, after, plain)).toEqual(result);
+  expect(genDiff(beforeIni, afterIni, plain)).toEqual(result);
 });
