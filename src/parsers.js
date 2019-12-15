20669b488dc0637f1e131e60d05fbc9a457ecfb1
Matveev S <matveevsa987@gmail.com>
2019-12-15 20:10:31 +0300

    refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -1,19 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (pathToConfig) => {
-  const absolutePath = path.isAbsolute(pathToConfig) ? pathToConfig : path.resolve(pathToConfig);
-  const dataConfig = fs.readFileSync(absolutePath, 'utf-8');
-  const typeConfig = path.extname(absolutePath).slice(1);
-
-  const parsers = {
-    json: JSON.parse,
-    yaml: yaml.safeLoad,
-    yml: yaml.safeLoad,
-    ini: ini.parse,
-  };
-
-  return parsers[typeConfig](dataConfig);
+const parsers = {
+  json: JSON.parse,
+  yaml: yaml.safeLoad,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
+
+export default (dataConfig, typeConfig) => parsers[typeConfig](dataConfig);
