464f3fd0b7aeaa7c91ff838f6a6f123312f669ee
Matveev S <matveevsa987@gmail.com>
2019-12-17 15:50:30 +0300

    refactoring

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -13,10 +13,12 @@ const stringify = (item, count) => {
 
 const statusActions = {
   unchanged: ({ name, value }, count) => `${getDepth(count + 2)}${name}: ${stringify(value, count + 2)}`,
-  hasChildren: (el, count, func) => `${getDepth(count + 2)}${el.name}: {\n${func(el.children, count + 4)}\n${getDepth(count + 2)}}`,
-  added: (el, count) => `${getDepth(count)}+ ${el.name}: ${stringify(el.value, count + 2)}`,
-  deleted: (el, count) => `${getDepth(count)}- ${el.name}: ${stringify(el.value, count + 2)}`,
-  changed: (el, count) => `${getDepth(count)}- ${el.name}: ${stringify(el.oldValue, count + 2)}\n${getDepth(count)}+ ${el.name}: ${stringify(el.newValue, count + 2)}`,
+  hasChildren: ({ name, children }, count, func) => `${getDepth(count + 2)}${name}: {\n${func(children, count + 4)}\n${getDepth(count + 2)}}`,
+  added: ({ name, value }, count) => `${getDepth(count)}+ ${name}: ${stringify(value, count + 2)}`,
+  deleted: ({ name, value }, count) => `${getDepth(count)}- ${name}: ${stringify(value, count + 2)}`,
+  changed: ({ name, oldValue, newValue }, count) => (
+    `${getDepth(count)}- ${name}: ${stringify(oldValue, count + 2)}\n${getDepth(count)}+ ${name}: ${stringify(newValue, count + 2)}`
+  ),
 };
 
 const renderTree = (data, count = 2) => data
