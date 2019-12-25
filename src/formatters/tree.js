869bd62afc647507c3c69480f43bf4cc55774cba
Matveev S <matveevsa987@gmail.com>
2019-12-25 16:10:04 +0300

    refactoring

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -1,28 +1,28 @@
 import { isObject } from 'lodash';
 
-const getCountSpace = (countSpace = 0) => ' '.repeat(countSpace);
+const getSpace = (count) => ' '.repeat(count);
 
-const stringify = (item, countSpace) => {
+const stringify = (item, depth) => {
   if (!isObject(item)) {
     return item;
   }
 
   return `{${Object.entries(item)
-    .map(([key, value]) => `\n${getCountSpace(countSpace + 4)}${key}: ${value}`).join('')}\n${getCountSpace(countSpace)}}`;
+    .map(([key, value]) => `\n${getSpace((depth + 1) * 4)}${key}: ${value}`).join('')}\n${getSpace(depth * 4)}}`;
 };
 
 const statusActions = {
-  unchanged: ({ name, value }, countSpace) => `${getCountSpace(countSpace + 2)}${name}: ${stringify(value, countSpace + 2)}`,
-  hasChildren: ({ name, children }, countSpace, func) => `${getCountSpace(countSpace + 2)}${name}: {\n${func(children, countSpace + 4)}\n${getCountSpace(countSpace + 2)}}`,
-  added: ({ name, value }, countSpace) => `${getCountSpace(countSpace)}+ ${name}: ${stringify(value, countSpace + 2)}`,
-  deleted: ({ name, value }, countSpace) => `${getCountSpace(countSpace)}- ${name}: ${stringify(value, countSpace + 2)}`,
-  changed: ({ name, oldValue, newValue }, countSpace) => (
-    `${getCountSpace(countSpace)}- ${name}: ${stringify(oldValue, countSpace + 2)}\n${getCountSpace(countSpace)}+ ${name}: ${stringify(newValue, countSpace + 2)}`
+  unchanged: ({ name, value }, depth) => `${getSpace(depth * 4)}${name}: ${stringify(value, depth)}`,
+  hasChildren: ({ name, children }, depth, func) => `${getSpace(depth * 4)}${name}: {\n${func(children, depth)}\n${getSpace(depth * 4)}}`,
+  added: ({ name, value }, depth) => `${getSpace(depth * 4 - 2)}+ ${name}: ${stringify(value, depth)}`,
+  deleted: ({ name, value }, depth) => `${getSpace(depth * 4 - 2)}- ${name}: ${stringify(value, depth)}`,
+  changed: ({ name, oldValue, newValue }, depth) => (
+    `${getSpace(depth * 4 - 2)}- ${name}: ${stringify(oldValue, depth)}\n${getSpace(depth * 4 - 2)}+ ${name}: ${stringify(newValue, depth)}`
   ),
 };
 
-const renderTree = (data, countSpace = 2) => data
-  .map((el) => statusActions[el.status](el, countSpace, renderTree))
+const renderTree = (data, depth = 0) => data
+  .map((el) => statusActions[el.status](el, depth + 1, renderTree))
   .join('\n');
 
 const renderFinishTree = (tree) => `{\n${renderTree(tree)}\n}`;
